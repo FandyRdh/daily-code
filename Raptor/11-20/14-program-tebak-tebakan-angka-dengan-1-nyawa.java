75a96f3260ed05e3edd71b48503cfc58f2c1e353
FandyRdh <framadhana012@gmail.com>
2025-04-30 21:55:15 +0700

    Daily Code Update - w18_day4_25-Raptor_1-20

---
+++ Raptor/11-20/14-program-tebak-tebakan-angka-dengan-1-nyawa.java
@@ -0,0 +1,27 @@
+/**
+  * NAME:
+  * DATE:
+  * FILE:
+  * COMMENTS:
+  */
+
+public class 14-program-tebak-tebakan-angka-dengan-1-nyawa extends eecs.Gui
+{
+   public static void main(String[] args)
+   {
+      // declare variables
+      ?? varRandom = ??;
+      
+      printLine(((Math.random() * 2) + 1));
+      varRandom = ((Math.random() * 2) + 1);
+      printLine(varRandom);
+      if (varRandom == 1)
+      {
+         printLine(1);
+      }
+      else
+      {
+         printLine("2");
+      }
+   } // close main
+} // close 14-program-tebak-tebakan-angka-dengan-1-nyawa
